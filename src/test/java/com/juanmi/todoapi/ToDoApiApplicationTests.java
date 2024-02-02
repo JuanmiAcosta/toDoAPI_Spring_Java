5efe255ccefe8e5681987bd737951f4e32e249fa
Juan Miguel Acosta Ortega <acostaojuanmi@gmail.com>
2024-02-02 13:38:11 +0100

    Empezando el proyecto

---
+++ src/test/java/com/juanmi/todoapi/ToDoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.juanmi.todoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
