633935ba32130465c6aaca2b8e7b47da5d378e2c
JuanmiAcosta <acostaojuanmi@gmail.com>
2024-02-06 16:05:09 +0100

    findAllTask hecha

--- src/main/java/com/juanmi/todoapi/controller/TaskController.java
+++ src/main/java/com/juanmi/todoapi/controller/TaskController.java
@@ -3,12 +3,11 @@ package com.juanmi.todoapi.controller;
 import com.juanmi.todoapi.persistence.entity.Task;
 import com.juanmi.todoapi.service.TaskService;
 import com.juanmi.todoapi.service.dto.TaskInDTO;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController //La capa de controlador sólo debe comuynicarse con la capa de servicio
+import java.util.List;
+
+@RestController //La capa de controlador sólo debe comunicarse con la capa de servicio
 @RequestMapping("/tasks")
 public class TaskController { //Para acceder a la docu swagger -> localhost:8080/swagger-ui.html
 
@@ -22,4 +21,9 @@ public class TaskController { //Para acceder a la docu swagger -> localhost:8080
     public Task createTask(@RequestBody TaskInDTO taskInDTO){
         return (this.taskService.createTask(taskInDTO));
     }
+
+    @GetMapping
+    public List<Task> findAllTask() { return this.taskService.findAllTask(); }
+
+
 }
--- src/main/java/com/juanmi/todoapi/service/TaskService.java
+++ src/main/java/com/juanmi/todoapi/service/TaskService.java
@@ -6,6 +6,8 @@ import com.juanmi.todoapi.persistence.repository.TaskRepository;
 import com.juanmi.todoapi.service.dto.TaskInDTO;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 // Inyección de dependencias y config. de componentes --> Se marca la clase como "bean de servicio"
 // contienen la lógica de negocio de la aplicación y se utilizan para encapsular y gestionar la
 // lógica relacionada con la manipulación de datos
@@ -22,5 +24,9 @@ public class TaskService {
     public Task createTask(TaskInDTO taskInDTO){
         Task task = mapper.map(taskInDTO);
         return (this.repository.save(task));
+    }
+
+    public List<Task> findAllTask(){
+        return (this.repository.findAll());
     }
 }
